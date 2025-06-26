5b3e52f15c328bc49ac662f8cdff818fbd54d636
Ken Ira Lacson <keniralacson@gmail.com>
2025-06-26 21:20:08 +0800

    refactor: optimize linear regression normal equation in example 16

--- 00-math-fundamentals/linear-algebra/ex16_linear_regression_normal_eq.py
+++ 00-math-fundamentals/linear-algebra/ex16_linear_regression_normal_eq.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-X = np.array([[500], [1200], [1500], [1800]])
+X = np.array([[500], [1200], [1500], [1800], [2100]])
 
 y = np.array([150000, 220000, 350000, 420000, 500000])
 
@@ -11,15 +11,12 @@ X_b = np.c_[np.ones((X.shape[0], 1)), X]
 
 print("X with bias term: \n", X_b)
 
-theta = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y)
+theta = np.linalg.solve(X_b.T.dot(X_b), X_b.T.dot(y))
 print("Computed Parameters (θ):\n", theta)
 
 
-new_house_area = np.array([[1000]])
-new_house_with_bias = np.c_[np.ones((1, 1)), new_house_area]
-predicted_price = new_house_with_bias.dot(theta)
+def predict(area, theta):
+    x_with_bias = np.c_[np.ones((1, 1)), np.array([[area]])]
+    return x_with_bias.dot(theta)
 
-print("Predicted price for 1000 sqft house: $%.2f" % predicted_price[0])
-
-print("Computed price for 1000 sqft house: $%.2f" % predicted_price[0])
-
+predicted_price = predict(1000, theta)
