023b207ee5e93d97dc5b835378fc594a04885b56
Ken Ira Lacson <keniralacson@gmail.com>
2025-06-26 21:33:58 +0800

    fix and refactor the ex15

--- 00-math-fundamentals/linear-algebra/ex15_pca_from_scratch.py
+++ 00-math-fundamentals/linear-algebra/ex15_pca_from_scratch.py
@@ -12,10 +12,10 @@ print("First 5 samples:", X[:5])
 X_centered = X - np.mean(X, axis=0)
 
 print("Shape after centering:", X_centered)
-print("Mean of first feature:", np.mean(X_centered[:0]))
+print("Mean of first feature:", np.mean(X_centered[:, 0]))
 
 
-cov_matrix = np.cov(X_centered, rowvar=False)
+cov_matrix = np.cov(X_centered, rowvar=False, bias=1)
 
 print("Shape of covariance matrix:", cov_matrix.shape)
 print("Covariance matrix:\n", cov_matrix)
@@ -25,7 +25,7 @@ eigen_values, eigen_vectors = np.linalg.eig(cov_matrix)
 
 print("Eigenvalues:", eigen_values)
 print("Eigenvectors shape:", eigen_vectors.shape)
-print("First Eigenvectors:", eigen_vectors[:, 5])
+print("First Eigenvectors:", eigen_vectors[:, 0])
 
 
 sorted_indices = np.argsort(eigen_values)[::-1]
@@ -39,6 +39,7 @@ print("Projection matrix shape:", projection_matrix.shape)
 print("Top 2 eigenvectors:\n", projection_matrix)
 
 X_pca = X_centered @ projection_matrix
+
 
 print("Transformed data shape:", X_pca.shape)
 print("First 5 transformed samples:\n", X_pca[:5])
