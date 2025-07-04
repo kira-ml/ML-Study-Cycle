42090fae4fe6d1f58d47956621c92c21c9d898e0
Ken Ira Lacson <keniralacson@gmail.com>
2025-07-05 02:31:48 +0800

    Update linear algebra draft and add requirements.txt

--- 00-math-fundamentals/linear-algebra/ex09draft.py
+++ 00-math-fundamentals/linear-algebra/ex09draft.py
@@ -1,14 +1,21 @@
 import numpy as np
-
-A = np.array([
-    [1, 2],
-    [3, 4]
-
-])
-
-b = np.array([8, 18])
+import matplotlib.pyplot as plt
 
 
+flips = np.random.randint(0, 2, size=10000)
 
-print(A.shape)
-print(b.shape)
+cumulative_heads = np.cumsum(flips)
+
+trial_numbers = np.arange(1, 10001)
+running_average = cumulative_heads / trial_numbers
+
+
+plt.figure(figsize=(10, 6))
+plt.plot(trial_numbers, running_average, label="Running Average")
+plt.axhline(y=0.5, color="r", linestyle="--", label="True Mean: (0.5)")
+plt.xlabel("Number of Trials")
+plt.ylabel("Average")
+plt.title("Law of Large Numbers: Convergence of Coin Flip Average to 0.5")
+plt.legend()
+plt.grid(True)
+plt.show()
